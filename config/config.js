cbac24ea03fb841fbe732eafa340433a2bd83473
ngtruongmetin <ngtruongmetin@gmail.com>
2025-11-25 21:22:08 +0700

    update config, footer, and tong_quan

--- config/config.js
+++ config/config.js
@@ -1,12 +1,12 @@
 require('dotenv').config();
 
 module.exports = {
-  PORT: process.env.PORT || 2026,
-  DB_NAME: process.env.DB_NAME || "hcm_ebook",
-  DB_USER: process.env.DB_USER || "root",
-  DB_PASS: process.env.DB_PASS || "",
-  DB_HOST: process.env.DB_HOST || "localhost",
-  ADMIN_USER: process.env.ADMIN_USER || "admin",
-  ADMIN_PASS: process.env.ADMIN_PASS || "admin",
-  SESSION_SECRET: process.env.SESSION_SECRET || "abcdefgthptntbdstartup26"
+  PORT: process.env.PORT,
+  DB_NAME: process.env.DB_NAME,
+  DB_USER: process.env.DB_USER,
+  DB_PASS: process.env.DB_PASS,
+  DB_HOST: process.env.DB_HOST,
+  ADMIN_USER: process.env.ADMIN_USER,
+  ADMIN_PASS: process.env.ADMIN_PASS,
+  SESSION_SECRET: process.env.SESSION_SECRET
 };
